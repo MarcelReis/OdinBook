1d127730d1d6d720b4ae4aee64c03f214eba9194
Marcelo Reis <oi@marcelreis.dev>
2021-01-09 23:25:31 -0300

    feat: Adiciona footer

--- src/components/Footer/Footer.styled.tsx
+++ src/components/Footer/Footer.styled.tsx
@@ -5,9 +5,20 @@ export const StyledSurface = styled(Surface)`
   position: absolute;
   bottom: 0;
   width: 100%;
-  height: 100px;
+  height: 64px;
 `;
 
 export const Spacer = styled.div`
-  height: 100px;
+  height: 64px;
+`;
+
+export const Text = styled.small`
+  display: inline-block;
+  width: 100%;
+  text-align: center;
+`;
+
+export const Link = styled.a`
+  color: inherit;
+  text-decoration: none;
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,10 +4,14 @@ import * as S from "./Footer.styled";
 
 const Footer = () => {
   return (
-    <>
-      <S.StyledSurface variant={2}>Footer</S.StyledSurface>
-      <S.Spacer />
-    </>
+    <S.Spacer>
+      <S.StyledSurface variant={2}>
+        <S.Text>
+          &copy; Copyright {new Date().getFullYear()},{" "}
+          <S.Link href="https://marcelreis.dev">Marcelo Reis</S.Link>
+        </S.Text>
+      </S.StyledSurface>
+    </S.Spacer>
   );
 };
 
