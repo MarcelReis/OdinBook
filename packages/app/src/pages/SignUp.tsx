277f61aa58f0e2441a7f3490066a3a7eb6b91c65
MarcelReis <oi@marcelreis.dev>
2021-04-17 14:44:16 -0300

    initial page layout

--- packages/app/src/pages/SignUp.tsx
+++ packages/app/src/pages/SignUp.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { View } from "@react-spectrum/view";
 import { Helmet } from "react-helmet";
+import { Button, Flex, TextField } from "@adobe/react-spectrum";
 
 const SignUpPage = () => {
   return (
-    <div>
+    <>
       <Helmet>
         <title>SignUp - Odinbook</title>
       </Helmet>
-    </div>
+      <View margin="auto" maxWidth="size-6000" padding="size-150">
+        <Flex direction="column" gap="size-150">
+          <TextField label="First Name" width="100%" isRequired />
+          <TextField label="Last Name" width="100%" isRequired />
+          <TextField label="Username" width="100%" isRequired />
+
+          <View paddingTop="size-300">
+            <Button variant="cta" width="100%">
+              Create
+            </Button>
+          </View>
+        </Flex>
+      </View>
+    </>
   );
 };
 
