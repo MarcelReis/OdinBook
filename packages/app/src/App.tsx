66672b31bd86e8a772b57bcb23f28990aa6020ef
MarcelReis <oi@marcelreis.dev>
2021-04-16 23:48:53 -0300

    basic nav bar

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider } from "@apollo/client";
+import { ApolloProvider, makeVar, useReactiveVar } from "@apollo/client";
 import { Provider, defaultTheme } from "@adobe/react-spectrum";
 
 import { client } from "./apollo";
@@ -10,6 +10,7 @@ import LoadingPage from "./pages/Loading";
 import LoginPage from "./pages/Login";
 import SignUpPage from "./pages/SignUp";
 import FeedPage from "./pages/Feed";
+import Appbar from "./components/Appbar";
 
 function Routes() {
   const { isLoading, isLogged, isRegistered } = useRegistration();
@@ -28,20 +29,32 @@ function Routes() {
   }
 
   if (!isRegistered) {
-    return <SignUpPage />;
+    return (
+      <>
+        <Appbar />
+        <SignUpPage />
+      </>
+    );
   }
 
   return (
-    <Switch>
-      <Route exact path="/" component={FeedPage} />
-    </Switch>
+    <>
+      <Appbar />
+      <Switch>
+        <Route exact path="/" component={FeedPage} />
+      </Switch>
+    </>
   );
 }
 
+export const colorSchemeVar = makeVar<"dark" | "light">("dark");
+
 const App = () => {
+  const colorScheme = useReactiveVar(colorSchemeVar);
+
   return (
     <ApolloProvider client={client}>
-      <Provider theme={defaultTheme} colorScheme="dark">
+      <Provider theme={defaultTheme} colorScheme={colorScheme}>
         <Router>
           <Routes />
         </Router>
--- packages/app/src/components/Appbar.tsx
+++ packages/app/src/components/Appbar.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Flex } from "@react-spectrum/layout";
+import { View } from "@react-spectrum/view";
+import { Button } from "@adobe/react-spectrum";
+import { useReactiveVar } from "@apollo/client";
+import Light from "@spectrum-icons/workflow/Light";
+import LogOut from "@spectrum-icons/workflow/LogOut";
+
+import { colorSchemeVar } from "../App";
+import { useAuth } from "../lib/odinAuth";
 
 function Appbar() {
-  return <div>asdf</div>;
+  const colorScheme = useReactiveVar(colorSchemeVar);
+  const { logout } = useAuth();
+
+  return (
+    <View backgroundColor="gray-200" height="size-600">
+      <Flex alignItems="center" height="size-600" gap="size-150">
+        <Button
+          variant="secondary"
+          onPress={() =>
+            colorSchemeVar(colorScheme === "dark" ? "light" : "dark")
+          }
+        >
+          <Light />
+        </Button>
+        <Button variant="negative" onPress={logout}>
+          <LogOut />
+        </Button>
+      </Flex>
+    </View>
+  );
 }
 
 export default Appbar;
