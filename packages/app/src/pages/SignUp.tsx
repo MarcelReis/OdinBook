3b396ec180b25a92da1064670bcb38fbaa0b768d
MarcelReis <oi@marcelreis.dev>
2021-04-17 14:49:08 -0300

    feat: adds title

--- packages/app/src/pages/SignUp.tsx
+++ packages/app/src/pages/SignUp.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View } from "@react-spectrum/view";
 import { Helmet } from "react-helmet";
-import { Button, Flex, TextField } from "@adobe/react-spectrum";
+import { Button, Flex, Heading, TextField } from "@adobe/react-spectrum";
 
 const SignUpPage = () => {
   return (
@@ -10,6 +10,8 @@ const SignUpPage = () => {
         <title>SignUp - Odinbook</title>
       </Helmet>
       <View margin="auto" maxWidth="size-6000" padding="size-150">
+        <Heading level={1}>Finish your SignUp</Heading>
+
         <Flex direction="column" gap="size-150">
           <TextField label="First Name" width="100%" isRequired />
           <TextField label="Last Name" width="100%" isRequired />
